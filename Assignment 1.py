6e5ac9ce72ee09caff3deda43997ce73200e6f4b
Adanna Ugwu <ugway001@mymail.unisa.edu.au>
2025-04-01 21:34:11 +1030

    Next step

--- Assignment 1.py
+++ Assignment 1.py
@@ -15,17 +15,64 @@ class MoveableObject:
         self.x = x
         self.y = y
 
-class Pirate:
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+
+
+class Pirate(MoveableObject):
+    treasure = []
     ''' 
     A class to represent a Pirate in the game.
     '''
-    def __init__ (self, name):
+    def __init__ (self, x, y, name):
+        super().__init__(x, y)
         self.name = name
 
-class Ship:
+    def get_treasure_value(self):
+        sum = 0
+        for treasure in self.treasure:
+            sum += treasure.get_value()
+        return sum
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+        return (self.x, self.y)
+
+    
+
+
+class Ship(MoveableObject):
+    treasure = []
     '''
     A class to represent a Ship in the game.
     '''
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+        return (self.x, self.y)
+    
+    def fire_cannon(self):
+        pass
+
+    def reload_cannon(self):
+        pass
+
+    def get_treasure_value(self):
+        sum = 0
+        for treasure in self.treasure:
+            sum += treasure.get_value()
+        return sum
+    
+    @classmethod
+    def add_treasure(self, treasure):
+        if isinstance(treasure, Treasure):
+            self.treasure.append(treasure)
+
 
 class Cannon:
     '''
@@ -47,7 +94,25 @@ class Treasure:
         '''  
         Constructs a Treasure with the argument name and value.  
         '''
-        self.name = name
-        self.value = value
+        self.set_name(name)
+        self.__value = value
 
- 
+    def set_name(self, name):
+        if type(name) == str and len(name) > 0:
+            self.__name = name
+        else:
+            print("Error: Name is invalid.")
+
+    def get_name(self):
+        return self.__name
+    
+    def get_value(self):
+        return self.__value
+    
+    def __str__(self):
+        return f"{self.__name} worth {self.__value} gold"
+
+
+
+treature = Treasure('Lesly', 400)
+treature.get_name()
