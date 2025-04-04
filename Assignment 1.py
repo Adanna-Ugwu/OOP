59eecbdfe7c048ab5603db28c114958c182f2038
Adanna Ugwu <ugway001@mymail.unisa.edu.au>
2025-04-04 12:00:01 +1030

    Creating the get_ship and set_ship method for Pirate

--- Assignment 1.py
+++ Assignment 1.py
@@ -14,24 +14,27 @@ class MoveableObject:
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
-        self.treasure = []
+        self.__treasure = []
 
     def move(self, x, y):
-        self.x += x
-        self.y += y
-        return (self.x, self.y)
+        self.__x += x
+        self.__y += y
+        return (self.__x, self.__y)
+    
+    def get_position(self):
+        return (self.__x, self.__y)
     
     @classmethod
     def add_treasure(self, treasure):
         if isinstance(treasure, Treasure):
-            self.treasure.append(treasure)
+            self.__treasure.append(treasure)
 
     def get_treasure_list(self):
-        return self.treasure
+        return self.__treasure
     
     def getTreasure_value(self):
         sum = 0
-        for treasure in self.treasure:
+        for treasure in self.__treasure:
             sum += treasure.get_value()
         return sum
 
@@ -40,10 +43,11 @@ class Pirate(MoveableObject):
     ''' 
     A class to represent a Pirate in the game.
     '''
-    def __init__ (self, x, y, name, ship=None):
+    def __init__ (self, x, y, name):
         super().__init__(x, y)
         self.__name = name
-        self.ship = ship
+        self.__ship = None
+
 
     def get_name(self):
         return self.__name
@@ -54,13 +58,23 @@ class Pirate(MoveableObject):
 
     def getTreasure_value(self):
         return super().getTreasure_value()
+    
+    def set_ship(self, s):
+        if s.get_position() == self.get_position():
+            self.__ship = s
+        else:
+            print("Error: They are not in the same position.")
+        # a = self.__x
+        # b = self.__y
+        # self.__ship = Ship(a, b) #composition
+
+    def get_ship(self):
+        return self.__ship
+
 
     def move(self, a, b):
         position = super().move(a, b)
-        ship_position = None
-        if isinstance(self.ship, Ship):
-            ship_position = self.ship.move(a, b)   
-        return (position, ship_position)
+        return (position)
     
     def add_treasure(self, treasure):
         if isinstance(treasure, Treasure):
@@ -82,7 +96,7 @@ class Ship(MoveableObject):
         super().__init__(x, y)
 
     def move(self, a, b):
-        position = Pirate.move(a, b)   
+        position = super().move(a, b)   
         return position
     
     def create_cannon(self):
@@ -117,19 +131,20 @@ class Cannon:
     '''
     A class to represent a Cannon in the game.
     '''
-    cannonBalls = []
+    
     def __init__(self):
-        pass
+        self.__cannonBalls = []
 
     def get_cannoballs(self):
-        return self.cannonBalls
+        return self.__cannonBalls
     
-    def set_cannonballs(self):
+    def set_cannonball(self, cb):
+        '''cb is a ccannonball'''
         pass
 
     def _fire(self):
-        if len(self.cannonBalls) == 1:
-            for cannonBall in self.cannonBalls:
+        if len(self.__cannonBalls) == 1:
+            for cannonBall in self.__cannonBalls:
                 return (float(cannonBall.get_damage()))
 
 
@@ -140,11 +155,15 @@ class CannonBall:
     '''
     A class to represent a CannonBall in the game.
     '''
+
     def __init__(self, damage):
+        '''
+        '''
         self.__damage = damage
         self.cannonball = 0
 
     def create_cannonball(self):
+        ''' '''
         self.cannonball += 1
         return self.cannonball
     
@@ -188,3 +207,16 @@ treature = Treasure('Lesly', 400)
 treature.get_name()
 
 print(treature)
+
+
+s=Ship(15, 15)
+p = Pirate(11, 11, "Potato")
+p.set_ship(s)
+
+s=Ship(11, 11)
+p = Pirate(11, 11, "Potato")
+p.set_ship(s)
+
+pos = p.move(1, 1)
+print(pos)
+print(p.get_ship().get_position())
